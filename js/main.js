388c9d4eaa9fb624e6be3fe40aa39b4298d3681b
eoff93 <bane.dev101@gmail.com>
2016-06-06 18:55:05 +0200

    Add a function to decrease break length

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ let totalBreakSeconds = breakTime * 60;
 $('#study-length').html(time);
 $('#break-length').html(breakTime);
 $('#timer').html(time + ':00');
+
+$('#break-increase').click(breakInc);
+
+function breakInc() {
+  if (breakTime < 15) {
+    breakTime++;
+    totalBreakSeconds += 60;
+    $('#break-length').html(breakTime);
+  }
+}
